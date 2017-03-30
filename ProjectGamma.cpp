5784ba905f7a256cc04c16a216ac3617f2f36317
bmmors <bmmors@gmail.com>
2017-03-30 14:21:24 -0700

    Policy Creation
    Creates 50 policies

--- ProjectGamma.cpp
+++ ProjectGamma.cpp
@@ -30,7 +30,7 @@ public:
 	double fitness;
 	vector<int> order;
 
-	void init();
+	void init(int n_city);
 	void calc_distance();
 };
 
@@ -63,15 +63,44 @@ void city::init() {
 	assert(cy <= 100);
 }
 
-void policy::init() {
+void policy::init(int n_city) {
+	//Create order from 0 to n_city
+	for (int i = 0; i < n_city; i++) {
+		order.push_back(i);
+	}
+	assert(order.size() == n_city);
+	//swap vector instances to get random order n_city number of times without duplicating cities
+	int rand1=0;
+	int rand2 = 0;
+	for (int i = 0; i < n_city; i++) {
+		rand1 = rand() % n_city;
+		rand2 = rand() % n_city;
+		//The first city always has to be 0
+		while (rand1 == 0){ rand1 = rand() % n_city; }
+		while (rand2 == 0) { rand2 = rand() % n_city; }
+		//swap around the other cities
+		swap(order[rand1], order[rand2]);
+	}
+	assert(order[0] == 0);
+	assert(order.size() == n_city);
 
+	///Cout order for Dubgging
+	/*cout << "Order:";
+	for (int i=0; i < n_city; i++)
+	{
+		cout << order[i] << "\t";
+	}
+	cout << endl;
+	*/
 }
 
 int main() {
 	srand(time(NULL));
+
 	//Define how many cities to travel too
 	int num_city = 5;
 	vector<city> cities;
+
 	//Create Cities
 	for (int i = 0; i < num_city; i++) {
 		city C;
@@ -79,8 +108,18 @@ int main() {
 		cities.push_back(C);
 	}
 	assert(cities.size() == num_city);
-	//Create 50 Random Policies 
 
+	//Define number of policies
+	int num_poly = 50;
+	vector<policy> policies;
+
+	//Create 50 Random Policies 
+	for (int i = 0; i < num_poly; i++) {
+		policy P;
+		P.init(num_city);
+		policies.push_back(P);
+	}
+	assert(policies.size() == num_poly);
 
 
 	return 0;
